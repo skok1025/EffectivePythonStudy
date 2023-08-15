57cac223d42c4fc3cb572f9d204f9496ecfe3378
shkim30 <shkim30cafe24corp.appleid.com@shkim30-macbook.local>
2023-08-15 14:24:08 +0900

    클래스 학습

--- 5_classes.py
+++ 5_classes.py
@@ -15,13 +15,15 @@ class SimpleGradebook:
         grades = self._grades[name]
         return sum(grades) / len(grades)
 
-book = SimpleGradebook()
-book.add_student('Isaac Newton')
-book.report_grade('Isaac Newton', 90)
-book.report_grade('Isaac Newton', 95)
-book.report_grade('Isaac Newton', 85)
 
-print(book.average_grade('Isaac Newton'))
+book = SimpleGradebook()
+book.add_student("Isaac Newton")
+book.report_grade("Isaac Newton", 90)
+book.report_grade("Isaac Newton", 95)
+book.report_grade("Isaac Newton", 85)
+
+print(book.average_grade("Isaac Newton"))
+
 
 class BySubjectGradebook:
     def __init__(self):
@@ -45,13 +47,15 @@ class BySubjectGradebook:
 
         return total / count
 
+
 book = BySubjectGradebook()
-book.add_student('Albert Einstein')
-book.report_grade('Albert Einstein', 'Math', 75)
-book.report_grade('Albert Einstein', 'Math', 65)
-book.report_grade('Albert Einstein', 'Gym', 90)
-book.report_grade('Albert Einstein', 'Gym', 95)
-print(book.average_grade('Albert Einstein'))
+book.add_student("Albert Einstein")
+book.report_grade("Albert Einstein", "Math", 75)
+book.report_grade("Albert Einstein", "Math", 65)
+book.report_grade("Albert Einstein", "Gym", 90)
+book.report_grade("Albert Einstein", "Gym", 95)
+print(book.average_grade("Albert Einstein"))
+
 
 class WeightedGradebook:
     def __init__(self):
@@ -82,15 +86,17 @@ class WeightedGradebook:
 
         return score_sum / score_count
 
-book = WeightedGradebook()
-book.add_student('Albert Einstein')
-book.report_grade('Albert Einstein', 'Math', 75, 0.05)
-book.report_grade('Albert Einstein', 'Math', 65, 0.15)
-book.report_grade('Albert Einstein', 'Math', 70, 0.80)
-book.report_grade('Albert Einstein', 'Gym', 100, 0.40)
-book.report_grade('Albert Einstein', 'Gym', 85, 0.60)
 
-print(book.average_grade('Albert Einstein'))
+book = WeightedGradebook()
+book.add_student("Albert Einstein")
+book.report_grade("Albert Einstein", "Math", 75, 0.05)
+book.report_grade("Albert Einstein", "Math", 65, 0.15)
+book.report_grade("Albert Einstein", "Math", 70, 0.80)
+book.report_grade("Albert Einstein", "Gym", 100, 0.40)
+book.report_grade("Albert Einstein", "Gym", 85, 0.60)
+
+print(book.average_grade("Albert Einstein"))
+
 
 class Subject:
     def __init__(self):
@@ -108,6 +114,7 @@ class Subject:
 
         return total / total_weight
 
+
 class Student:
     def __init__(self):
         self._subjects = defaultdict(Subject)
@@ -124,6 +131,7 @@ class Student:
 
         return total / count
 
+
 class Gradebook:
     def __init__(self):
         self._students = defaultdict(Student)
@@ -131,15 +139,201 @@ class Gradebook:
     def get_student(self, name):
         return self._students[name]
 
+
 book = Gradebook()
-albert = book.get_student('Albert Einstein')
-math = albert.get_subject('Math')
+albert = book.get_student("Albert Einstein")
+math = albert.get_subject("Math")
 math.report_grade(75, 0.05)
 math.report_grade(65, 0.15)
 math.report_grade(70, 0.80)
-gym = albert.get_subject('Gym')
+gym = albert.get_subject("Gym")
 gym.report_grade(100, 0.40)
 gym.report_grade(85, 0.60)
 print(albert.average_grade())
 
 
+current = {"green": 12, "blue": 3}
+increments = [
+    ("red", 5),
+    ("blue", 17),
+    ("orange", 9),
+]
+
+def log_missing():
+    print("Key added")
+    return 0
+
+result = defaultdict(log_missing, current)
+print("Before:", dict(result))
+
+for key, amount in increments:
+    result[key] += amount
+print("After:", dict(result))
+
+def increment_with_report(current, increments):
+    added_count = 0
+
+    def missing():
+        nonlocal added_count
+        added_count += 1
+        return 0
+
+    result = defaultdict(missing, current)
+
+    for key, amount in increments:
+        result[key] += amount
+
+    return result, added_count
+
+
+class CountMissing:
+    def __init__(self):
+        self.added = 0
+
+    def missing(self):
+        self.added += 1
+        return 0
+
+counter = CountMissing()
+result = defaultdict(counter.missing, current)
+for key, amount in increments:
+    result[key] += amount
+
+assert counter.added == 2
+
+
+class BetterCountMissing:
+    def __init__(self):
+        self.added = 0
+
+    def __call__(self):
+        self.added += 1
+        return 0
+
+counter = BetterCountMissing()
+assert callable(counter)
+
+counter()
+counter()
+counter()
+
+print(counter.added)
+
+counter = BetterCountMissing()
+result = defaultdict(counter, current)
+for key, amount in increments:
+    result[key] += amount
+assert counter.added == 2
+
+class InputData:
+    def read(self):
+        raise NotImplementedError
+
+
+class PathInputData(InputData):
+    def __init__(self, path):
+        super().__init__()
+        self.path = path
+
+    def read(self):
+        with open(self.path) as f:
+            return f.read()
+
+class Worker:
+    def __init__(self, input_data):
+        self.input_data = input_data
+        self.result = None
+
+    def map(self):
+        raise NotImplementedError
+
+    def reduce(self, other):
+        raise NotImplementedError
+
+
+class LineCountWorker(Worker):
+    def map(self):
+        data = self.input_data.read()
+        self.result = data.count("\n")
+
+    def reduce(self, other):
+        self.result += other.result
+
+
+class MyBaseClass:
+    def __init__(self, value):
+        self.value = value
+
+class MyChildClass(MyBaseClass):
+    def __init__(self):
+        MyBaseClass.__init__(self, 5)
+
+
+class TimesTwo:
+    def __init__(self):
+        self.value *= 2
+
+
+class PlusFive:
+    def __init__(self):
+        self.value += 5
+
+
+class OneWay(MyBaseClass, TimesTwo, PlusFive):
+    def __init__(self, value):
+        MyBaseClass.__init__(self, value)
+        TimesTwo.__init__(self)
+        PlusFive.__init__(self)
+
+foo = OneWay(5)
+print("First ordering is (5 * 2) + 5 =", foo.value)
+
+
+class AnotherWay(MyBaseClass, PlusFive, TimesTwo):
+    def __init__(self, value):
+        MyBaseClass.__init__(self, value)
+        TimesTwo.__init__(self)
+        PlusFive.__init__(self)
+
+
+bar = AnotherWay(5)
+print("Second ordering still is", bar.value)
+
+
+class TimesSeven(MyBaseClass):
+    def __init__(self, value):
+        MyBaseClass.__init__(self, value)
+        self.value *= 7
+
+
+class PlusNine(MyBaseClass):
+    def __init__(self, value):
+        MyBaseClass.__init__(self, value)
+        self.value += 9
+
+class ThisWay(TimesSeven, PlusNine):
+    def __init__(self, value):
+        TimesSeven.__init__(self, value)
+        PlusNine.__init__(self, value)
+
+foo = ThisWay(5)
+print("Should be (5 * 7) + 9 =", foo.value)
+
+
+class TimesSevenCorrect(MyBaseClass):
+    def __init__(self, value):
+        super().__init__(value)
+        self.value *= 7
+
+
+class PlusNineCorrect(MyBaseClass):
+    def __init__(self, value):
+        super().__init__(value)
+        self.value += 9
+
+class GoodWay(TimesSevenCorrect, PlusNineCorrect):
+    def __init__(self, value):
+        super().__init__(value)
+
+foo = GoodWay(5)
+print("Should be 7 * (5 + 9) =", foo.value)
