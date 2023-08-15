5932f69a3e27891d7aa4cf41239d8f6f570e0cbb
shkim30 <shkim30cafe24corp.appleid.com@shkim30-macbook.local>
2023-08-15 14:16:58 +0900

    클래스 학습

---
+++ 5_classes.py
@@ -0,0 +1,145 @@
+from collections import defaultdict
+
+
+class SimpleGradebook:
+    def __init__(self):
+        self._grades = {}
+
+    def add_student(self, name):
+        self._grades[name] = []
+
+    def report_grade(self, name, score):
+        self._grades[name].append(score)
+
+    def average_grade(self, name):
+        grades = self._grades[name]
+        return sum(grades) / len(grades)
+
+book = SimpleGradebook()
+book.add_student('Isaac Newton')
+book.report_grade('Isaac Newton', 90)
+book.report_grade('Isaac Newton', 95)
+book.report_grade('Isaac Newton', 85)
+
+print(book.average_grade('Isaac Newton'))
+
+class BySubjectGradebook:
+    def __init__(self):
+        self._grades = {}
+
+    def add_student(self, name):
+        self._grades[name] = defaultdict(list)
+
+    def report_grade(self, name, subject, grade):
+        by_subject = self._grades[name]
+        grade_list = by_subject[subject]
+        grade_list.append(grade)
+
+    def average_grade(self, name):
+        by_subject = self._grades[name]
+        total, count = 0, 0
+
+        for grades in by_subject.values():
+            total += sum(grades)
+            count += len(grades)
+
+        return total / count
+
+book = BySubjectGradebook()
+book.add_student('Albert Einstein')
+book.report_grade('Albert Einstein', 'Math', 75)
+book.report_grade('Albert Einstein', 'Math', 65)
+book.report_grade('Albert Einstein', 'Gym', 90)
+book.report_grade('Albert Einstein', 'Gym', 95)
+print(book.average_grade('Albert Einstein'))
+
+class WeightedGradebook:
+    def __init__(self):
+        self._grades = {}
+
+    def add_student(self, name):
+        self._grades[name] = defaultdict(list)
+
+    def report_grade(self, name, subject, score, weight):
+        by_subject = self._grades[name]
+        grade_list = by_subject[subject]
+        grade_list.append((score, weight))
+
+    def average_grade(self, name):
+        by_subject = self._grades[name]
+
+        score_sum, score_count = 0, 0
+
+        for subject, scores in by_subject.items():
+            subject_avg, total_weight = 0, 0
+
+            for score, weight in scores:
+                subject_avg += score * weight
+                total_weight += weight
+
+            score_sum += subject_avg / total_weight
+            score_count += 1
+
+        return score_sum / score_count
+
+book = WeightedGradebook()
+book.add_student('Albert Einstein')
+book.report_grade('Albert Einstein', 'Math', 75, 0.05)
+book.report_grade('Albert Einstein', 'Math', 65, 0.15)
+book.report_grade('Albert Einstein', 'Math', 70, 0.80)
+book.report_grade('Albert Einstein', 'Gym', 100, 0.40)
+book.report_grade('Albert Einstein', 'Gym', 85, 0.60)
+
+print(book.average_grade('Albert Einstein'))
+
+class Subject:
+    def __init__(self):
+        self._grades = []
+
+    def report_grade(self, score, weight):
+        self._grades.append((score, weight))
+
+    def average_grade(self):
+        total, total_weight = 0, 0
+
+        for score, weight in self._grades:
+            total += score * weight
+            total_weight += weight
+
+        return total / total_weight
+
+class Student:
+    def __init__(self):
+        self._subjects = defaultdict(Subject)
+
+    def get_subject(self, name):
+        return self._subjects[name]
+
+    def average_grade(self):
+        total, count = 0, 0
+
+        for subject in self._subjects.values():
+            total += subject.average_grade()
+            count += 1
+
+        return total / count
+
+class Gradebook:
+    def __init__(self):
+        self._students = defaultdict(Student)
+
+    def get_student(self, name):
+        return self._students[name]
+
+book = Gradebook()
+albert = book.get_student('Albert Einstein')
+math = albert.get_subject('Math')
+math.report_grade(75, 0.05)
+math.report_grade(65, 0.15)
+math.report_grade(70, 0.80)
+gym = albert.get_subject('Gym')
+gym.report_grade(100, 0.40)
+gym.report_grade(85, 0.60)
+print(albert.average_grade())
+
+
