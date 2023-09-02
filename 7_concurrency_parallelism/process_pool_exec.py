e9d8858b8f67839a9f8e33ccead351ad186103d5
shkim30 <shkim30cafe24corp.appleid.com@shkim30-macbook.local>
2023-09-03 08:57:29 +0900

    동시성과 병렬성 학습

--- 7_concurrency_parallelism/process_pool_exec.py
+++ 7_concurrency_parallelism/process_pool_exec.py
@@ -1,11 +1,17 @@
-from concurrent.futures import ProcessPoolExecutor
+import concurrent.futures
 
 def my_function(x):
     return x * x
 
 data = [1, 2, 3, 4, 5]
 
-with ProcessPoolExecutor() as executor:
-    results = list(executor.map(my_function, data))
-ㅐㄱ
-print(results)
+
+def main():
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = list(executor.map(my_function, data))
+
+    print(results)
+
+
+if __name__ == '__main__':
+    main()
